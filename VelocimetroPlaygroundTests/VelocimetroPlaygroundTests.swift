bde8093f9718d0c3c30d448e49be6a211db6f485
luz karime baez galvis <luzkarimebaezgalvis@MBP-de-luz.home>
2016-11-03 22:23:13 -0500

    Initial Commit

---
+++ VelocimetroPlaygroundTests/VelocimetroPlaygroundTests.swift
@@ -0,0 +1,36 @@
+//
+//  VelocimetroPlaygroundTests.swift
+//  VelocimetroPlaygroundTests
+//
+//  Created by luz karime baez galvis on 3/11/16.
+//  Copyright (c) 2016 Kew-Baez Media. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class VelocimetroPlaygroundTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
